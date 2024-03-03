f7d5009ef009203d00faebc9b93b16a13727087f
YTAlexVid <saharchuk8@gmail.com>
2024-03-03 18:46:21 +0300

    feat: added visual response to entering email

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,15 +1,20 @@
 const subscribe_field = document.querySelector('#footer__input-field');
 const subscribe_button = document.querySelector('.footer__input-button');
+const subscribe_result = document.querySelector('.footer__input-result');
 const email_regex = /^[\w\-\.]+@[\w\-]+\.[\w\-]{2,4}$/g
 subscribe_field.value="";
 subscribe_field.addEventListener("input", e=>{
     let input_text = e.target.value;
     if(input_text.length===0){
         subscribe_button.disabled=true;
-        
+        subscribe_field.style.borderColor='rgb(254, 143, 24)';
+        subscribe_result.style.display='none';
         return;
     }
     let isValid = input_text.match(email_regex);
     subscribe_button.disabled=!isValid;
-    
+    subscribe_field.style.borderColor = (isValid?'green':'red');
+    subscribe_result.style.display='block';
+    subscribe_result.style.color=(isValid?'green':'red');
+    subscribe_result.textContent = (isValid?'Success':'Invalid email');
 })
